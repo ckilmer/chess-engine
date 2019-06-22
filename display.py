3c8c131994f9928aef2f9ec541222d60ef10fbec
Corbin <corbinkilmer@gmail.com>
2019-06-22 14:48:05 -0400

    Added status message to display class

--- display.py
+++ display.py
@@ -6,37 +6,43 @@ class Display():
         self.root = root
         self.game = game
         self.square_size = 25*3
-        self.canvas_height = 1000
-        self.canvas_width = 1000
+        self.canvas_height = 8*self.square_size + 50
+        self.canvas_width = 8*self.square_size + 50
         self.margin_left = int((self.canvas_width - 8*self.square_size)/2)
         self.margin_top = int((self.canvas_height - 8*self.square_size)/2)
         self.current_piece = None
         self.startcoord = None
         self.currentcoord = None
         self.canvas = tkinter.Canvas(master=self.root, bg="#969696", height=self.canvas_height, width=self.canvas_width)
-        self.canvas.pack(fill='both')
-        self.pieceimg = {
-            ('White', Pieces.KING):tkinter.PhotoImage(file='images/white_king.png').subsample(4,4),
-            ('White', Pieces.QUEEN):tkinter.PhotoImage(file='images/white_queen.png').subsample(4,4),
-            ('White', Pieces.BISHOP):tkinter.PhotoImage(file='images/white_bishop.png').subsample(4,4),
-            ('White', Pieces.KNIGHT):tkinter.PhotoImage(file='images/white_knight.png').subsample(4,4),
-            ('White', Pieces.ROOK):tkinter.PhotoImage(file='images/white_rook.png').subsample(4,4),
-            ('White', Pieces.PAWN):tkinter.PhotoImage(file='images/white_pawn.png').subsample(4,4),
-            ('Black', Pieces.KING):tkinter.PhotoImage(file='images/black_king.png').subsample(4,4),
-            ('Black', Pieces.QUEEN):tkinter.PhotoImage(file='images/black_queen.png').subsample(4,4),
-            ('Black', Pieces.BISHOP):tkinter.PhotoImage(file='images/black_bishop.png').subsample(4,4),
-            ('Black', Pieces.KNIGHT):tkinter.PhotoImage(file='images/black_knight.png').subsample(4,4),
-            ('Black', Pieces.ROOK):tkinter.PhotoImage(file='images/black_rook.png').subsample(4,4),
-            ('Black', Pieces.PAWN):tkinter.PhotoImage(file='images/black_pawn.png').subsample(4,4),
-        }
+        self.canvas.tag_bind('piece', '<ButtonPress-1>', self.select_piece)
+        self.canvas.tag_bind('piece', '<B1-Motion>', self.move_piece)
+        self.canvas.tag_bind('piece', '<ButtonRelease-1>', self.snap_piece)
+        self.text = tkinter.Entry(root, justify='center', font=("Times", "50", "bold"))
+        self.canvas.pack(fill='x')
+        self.text.pack(fill='x')
+        self.pieceimg = {}
+        for color in ['White', 'Black']:
+            for piece in Pieces:
+                self.pieceimg[(color, piece)] = tkinter.PhotoImage(file='images/{}_{}.png'.format(color, piece.name)).subsample(4,4)
         self.img_size = self.pieceimg[('White', Pieces.KING)].width()
         print(self.img_size)
         self.redraw()
+
+
+
+
     def board_to_canvas_coords(self, x, y):
         return self.margin_left + (self.square_size+1)/2 + x*self.square_size, self.margin_top + (self.square_size+1)/2 + (7-y)*self.square_size
 
     def canvas_to_board_coords(self, x, y):
         return round((x - self.margin_left - (self.square_size+1)/2)/self.square_size), round(7 - (y - self.margin_left - (self.square_size+1)/2)/self.square_size)
+
+    def draw_message(self):
+        self.text.delete(0, tkinter.END)
+        if not self.game.isOver():
+            self.text.insert(tkinter.END, '{} to move'.format(self.game.currentplayer().color))
+        else:
+            self.text.insert(tkinter.END, '{} wins!'.format(self.game.otherplayer().color))
 
     def draw_blank_board(self):
         for i in range(8):
@@ -69,9 +75,6 @@ class Display():
             pass
         else:
             print(self.startcoord, (j, i))
-            #dx = self.board_to_canvas_coords(i, j)[0] - self.currentcoord[0]
-            #dy = self.board_to_canvas_coords(i, j)[1] - self.currentcoord[1]
-            #self.canvas.move(self.current_piece, dx, dy)
             self.game.move(self.startcoord, (j, i))
         self.redraw()
         self.current_piece = None
@@ -81,19 +84,17 @@ class Display():
     def redraw(self):
         self.canvas.delete('all')
         self.draw_blank_board()
+        self.draw_message()
         for i in range(8):
             for j in range(8):
                 piece = self.game.board[i,j]
                 if piece is not None:
                     img = self.pieceimg[(piece.player.color, piece.kind)]
                     if piece.player == self.game.currentplayer():
-                        tags = 'piece'
+                        tags = 'piece' if not self.game.isOver() else None
                     else:
                         tags = None
                     self.draw_piece(img, j, i, tags)
-        if not self.game.isOver():
-            self.canvas.tag_bind('piece', '<ButtonPress-1>', self.select_piece)
-            self.canvas.tag_bind('piece', '<B1-Motion>', self.move_piece)
-            self.canvas.tag_bind('piece', '<ButtonRelease-1>', self.snap_piece)
-        else:
-            print('Game is over!')
+
+import tkinter
+dir(tkinter.Canvas.create_text)
