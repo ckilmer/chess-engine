f4b14a30e8a65630e417adc1bbdc6c47e5eb2724
Corbin <corbinkilmer@gmail.com>
2019-06-23 17:40:26 -0400

    Reformatted game class

--- game.py
+++ game.py
@@ -8,32 +8,46 @@ class Game(object):
         self.players = (Player('White'), Player('Black'))
         self.moves = 0
         self.board.setup(self.players)
-        for i in range(2):
+        for player in self.players:
             for row in self.board.squares:
                 for square in row:
-                    if square is not None and square.player is self.players[i]:
-                        self.players[i].pieces.append(square)
+                    if square is not None and square.player is player:
+                        player.pieces.append(square)
                         if square.kind == Pieces.KING:
-                            self.players[i].king = square
+                            player.king = square
 
     def currentplayer(self):
+        '''
+        Returns player object that needs to move
+        '''
         return self.players[self.moves % 2]
 
     def otherplayer(self):
+        '''
+        Returns the Player object that is not moving
+        '''
         return self.players[(self.moves+1) % 2]
 
     def isLegalmove(self, startPostion, endPosition):
+        '''
+        Checks if resulting move puts current player's king in check,
+        if so rolls back the move
+        '''
         isLegal = False
         capture = self.board.move(self.currentplayer(), self.otherplayer(), startPostion, endPosition)
         if capture is not None:
             #index 1 of a move is the end position
-            if self.currentplayer().king.position not in [move[1] for move in self.otherplayer().allMoves()]:
+            attackedSquares = [move[1] for move in self.otherplayer().allMoves()]
+            if self.currentplayer().king.position not in attackedSquares:
                 isLegal = True
             self.board.rollbackLastMove(capture)
         return isLegal
 
     
     def getLegalMoves(self):
+        '''
+        Returns a list of the current player's legal moves
+        '''
         legalMoves = []
         for move in self.currentplayer().allMoves():
             if self.isLegalmove(*move):
@@ -41,8 +55,12 @@ class Game(object):
         return legalMoves
 
     def isOver(self):
+        '''
+        Returns boolean whether self.getLegalMoves is empty
+        '''
         ended = len(self.getLegalMoves()) == 0
         return ended
+
     def move(self, startPostion, endPosition):
         if startPostion is None and endPosition is None:
             return False
@@ -53,5 +71,5 @@ class Game(object):
             self.moves += 1
             return True
         else:
-            print('Not a legal move, king is in check')
+            print(f'{move} is not legal')
             return False
